03e046d91e88f0aff428e3bb6944a7fccd5eeb18
EL\Mijburgh <Mijburgh@energyline.net>
2021-08-03 16:19:49 +1200

    Add functional password generator

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,48 @@
-export default function HomePage () {
-  return <div>Hello world!</div>
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+export default function HomePage() {
+  const [pword, setPword] = useState("");
+  // const [pwlength, setLength] = useState(8);
+
+  const charlist =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  function genPword() {
+    // charlist.length = 72
+    // use for loop to build 16 char string using charlist and return
+    let newPW = "";
+    for (let i = 0; i < 16; i++) {
+      let randNum = Math.floor(Math.random() * charlist.length);
+      newPW += charlist.substring(randNum, randNum + 1);
+    }
+    console.log(newPW);
+    return newPW;
+  }
+
+  function handleGen() {
+    return setPword(genPword);
+  }
+
+  function handleCopy() {
+    // const copyText = pword;
+    // copyText.select();
+    // document.execCommand("copy");
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <h1>Password generator!</h1>
+        <div>
+          <button type="button" onClick={handleGen}>
+            Generate
+          </button>
+          <button type="button" /* onClick={handleCopy} */>Copy text</button>
+        </div>
+        <h3>{pword}</h3>
+      </main>
+    </div>
+  );
 }
